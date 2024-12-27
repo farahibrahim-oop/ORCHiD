cdddbc8b07d655d93b757e6826a39a6f0bdc0ce6
farah ibrahim <codebraver56@codebrave.org>
2024-12-27 04:26:23 -0800

    my final project

---
+++ script.js
@@ -0,0 +1,25 @@
+function showSlides() {
+    const slides = document.querySelectorAll('.slider img');
+    if (slideIndex >= slides.length) {
+      slideIndex = 0;
+    }
+    if (slideIndex < 0) {
+      slideIndex = slides.length - 1;
+    }
+    slides.forEach((slide) => {
+      slide.style.display = 'none';
+    });
+    slides[slideIndex].style.display = 'block';
+  }
+  
+  function nextSlide() {
+    slideIndex++;
+    showSlides();
+  }
+  
+  function prevSlide() {
+    slideIndex--;
+    showSlides();
+  }
+  
+  showSlides();
